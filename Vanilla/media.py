40aa4355d3a862620b142073f4f3b8dd327b780f
Isaac Evans <mightysteve101@Isaacs-MacBook-Pro.local>
2024-02-05 19:53:30 -0700

    Definitly connected dropbox keys, now how do i connect it to my website

--- Vanilla/media.py
+++ Vanilla/media.py
@@ -1,3 +1,17 @@
+# Import the Dropbox module
 import dropbox
-dbx = dropbox.Dropbox('sl.BvG_9ytyTzoX66W1p9Q4ctbLb6UWzuiYZ_H6wx4YzDTesQ6Xlqw9tot6h5_-Ptsa-rTNINS_LQ5-UWRR80Eu4Eu1Z-98qvV_sOFevON-Vi5hnFe9B2ljsz9kA8rOabpFvZb4w-qzfDEIGT5QBLng')
-dbx.users_get_current_account()
+
+#Create an instance of the Dropbox class and pass the access token
+dbx = dropbox.Dropbox('sl.BvGc_H2n8udfW7wNNwf4TgPTE8vsuF-Cv2HCIIEISB4sU1WHAm6yQy4UPsUqhuClmjG75WEjkcKIb75Lyjou6bE7WbUGiQWFI-y1oR_n1LXKOzAJ9Y8-gUbFzGVDQa_WtS9p3vNGn0DFGHTDZQgoYAA')
+
+# Get information about the current user's Dropbox account
+account_info = dbx.users_get_current_account()
+
+# Print the account information
+print("Account ID:", account_info.account_id)
+print("Account Name:", account_info.name.display_name)
+print("Account Email:", account_info.email)
+
+# List files in the root folder and print their names
+for entry in dbx.files_list_folder('').entries:
+    print(entry.name)
