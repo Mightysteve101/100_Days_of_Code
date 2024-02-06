6c1e28200b577ed2fe09230113e376c9f5c7593a
Isaac Evans <mightysteve101@Isaacs-MacBook-Pro.local>
2024-02-05 21:54:17 -0700

    Took off the key from source code. Made an environment variable that i will have to update when I exit the terminal, but the key in on my computer instead of being shown off to the world. I will figure out a better way to do this later

--- Vanilla/app.py
+++ Vanilla/app.py
@@ -1,22 +1,18 @@
 # Import the Dropbox module
+import os
 import dropbox
 from flask import Flask, render_template
 
-#Create an instance of the Dropbox class and pass the access token
-dbx = dropbox.Dropbox('sl.BvHVV6vfcB0VlPCPAgC7dhnYgEXmOeutPQlQ5rsPq1DYeYlkwPAQej2C9hmj8uI8ABZu7Ez958dJ-4FN5DxqXaFqZIvqxNiuvmgj2OGTydwnJw9xN4IYTmwD6S0B3UrCImGu59bIkMRePuFvOnVJdlQ')
-
+#Environment variable that grabs temperary token. Will need to update token every terminal use.
+access_key = os.environ.get('ACCESS_KEY')
 # Create an instance of the Flask class
 app = Flask(__name__)
 
-# List files in the root folder and print their names
-for entry in dbx.files_list_folder('').entries:
-    print(entry.name)
-
 @app.route('/')
 def index():
-    # List files in the root folder
-    files = [entry.name for entry in dbx.files_list_folder('').entries]
-    return render_template('index.html', files=files)
+    # Grabs the video and places it in index.html
+    video_path = '/static/videos/bleed_the_kid_laroi.mp4'
+    return render_template('index.html', video_path=video_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
