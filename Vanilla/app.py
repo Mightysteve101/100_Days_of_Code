df199dd63134b39d682e781759e0b893402713e8
Isaac Evans <mightysteve101@Isaacs-MacBook-Pro.local>
2024-02-07 18:50:16 -0700

    Experimented with a couple things. Going to look into storage later. Right now i will need to upload covers onto youtube. But there was another issue which was copywrite. I will need to purchase the rights to each song before I can continue

--- Vanilla/app.py
+++ Vanilla/app.py
@@ -1,18 +1,12 @@
-# Import the Dropbox module
 import os
-import dropbox
 from flask import Flask, render_template
 
-#Environment variable that grabs temperary token. Will need to update token every terminal use.
-access_key = os.environ.get('ACCESS_KEY')
 # Create an instance of the Flask class
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Grabs the video and places it in index.html
-    video_path = '/static/videos/bleed_the_kid_laroi'
-    return render_template('index.html', video_path=video_path)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
